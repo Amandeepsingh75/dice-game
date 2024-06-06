22c8c34af2be809c4e17e0b42d109600c7554bf1
Amandeep75 <Amandeep75318gmail.com>
2024-06-06 11:54:04 +0530

    other component  , and build app update

--- src/Components/rollingDice.tsx
+++ src/Components/rollingDice.tsx
@@ -1,17 +1,111 @@
-/* eslint-disable prettier/prettier */
-import { Image, ImageSourcePropType, View } from 'react-native';
-import React, { PropsWithChildren } from 'react';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import DiceOne from '../../assets/One.png';
+import DiceTwo from '../../assets/Two.png';
+import DiceThree from '../../assets/Three.png';
+import DiceFour from '../../assets/Four.png';
+import DiceFive from '../../assets/Five.png';
+import DiceSix from '../../assets/Six.png';
+import RollDiceImage from './RollDiceImage';
 
-type DiceProps = PropsWithChildren<{
-  imageUrl: ImageSourcePropType;
-}>;
+const App = () => {
+  const [rollDice, setRollDice] = useState(DiceOne);
+  const [changeTurns, setChangeTurns] = useState(false)
+  const [opponentScore, setOpponentScore] = useState<number[]>([])
+  const [YourScore, setYourScore] = useState<number[]>([])
 
-const rollingDice = ({ imageUrl }: DiceProps): JSX.Element => {
+  const handleRollDice = () => {
+    let randomNumber = Math.round(Math.random() * 6);
+    switch (randomNumber) {
+      case 1:
+        setRollDice(DiceOne);
+        break;
+      case 2:
+        setRollDice(DiceTwo);
+        break;
+      case 3:
+        setRollDice(DiceThree);
+        break;
+      case 4:
+        setRollDice(DiceFour);
+        break;
+      case 5:
+        setRollDice(DiceFive);
+        break;
+      case 6:
+        setRollDice(DiceSix);
+        break;
+
+      default:
+        setRollDice(DiceOne);
+        break;
+    }
+    if (!changeTurns) {
+      setOpponentScore([...opponentScore, randomNumber])
+    } else {
+      setYourScore([...YourScore, randomNumber])
+    }
+    setChangeTurns(!changeTurns)
+  };
+
+  useEffect(() => {
+    if (opponentScore.length == 3 && YourScore.length == 3) {
+      calculateResult(opponentScore, YourScore)
+    }
+  }, [changeTurns])
+
+  const calculateResult = (Oppos: number[], Yours: number[]) => {
+    const opp = Oppos.reduce((acc, cur) => acc + cur)
+    const your = Yours.reduce((acc, cur) => acc + cur)
+    { opp > your ? Alert.alert('Winner : Team A Wins', `Team A score: ${opp} Team B score ${your}`) : Alert.alert('Winner : Team B Wins', `Team A score: ${opp} Team B score ${your}`) }
+    setOpponentScore([])
+    setYourScore([])
+    setChangeTurns(false)
+  }
   return (
-    <View>
-      <Image source={imageUrl} />
+    <View style={styles.diceContainer}>
+      <View>
+        <TouchableOpacity onPress={handleRollDice} disabled={changeTurns && true}>
+          <Text style={[styles.btnText, { marginBottom: 20 }, changeTurns ? { color: 'grey', opacity: 0.4 } : { color: 'green' }]}>Roll the Dice</Text>
+        </TouchableOpacity>
+        {!changeTurns && <Text style={styles.yourTurnText} >Team A Your Turn</Text>}
+      </View>
+      <Text>{opponentScore.slice(0, 5).join(',')}</Text>
+      <RollDiceImage imageUrl={rollDice} />
+      <Text>{YourScore.join(',')}</Text>
+
+      <View>
+        {changeTurns && <Text style={styles.yourTurnText}> Team B Your Turn</Text>}
+        <TouchableOpacity onPress={handleRollDice} disabled={!changeTurns && true}>
+          <Text style={[styles.btnText, { marginTop: 20 }, !changeTurns ? { backgroundColor: 'grey', opacity: 0.4 } : { color: 'green' }]}>Roll the Dice</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default rollingDice;
+export default App;
+
+const styles = StyleSheet.create({
+  diceContainer: {
+    marginVertical: 50,
+    marginHorizontal: 10,
+    flex: 1,
+    justifyContent: 'space-between',
+
+    alignItems: 'center',
+  },
+  btnText: {
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    borderRadius: 4,
+  },
+  yourTurnText: {
+    textAlign: 'center',
+    fontSize: 14,
+    fontWeight: '600',
+    color: 'green',
+  }
+});
